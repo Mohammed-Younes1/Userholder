8f8040436e51b36616b2bc725370c0df81c4ee4b
Mohammed Younes <mohammed.alyounes@gmail.com>
2024-02-13 17:02:46 +0300

    starting the project

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+    return (
+      <div className="text-amber-900">
+        this is the navbar
+        <h1 className="text-3xl font-bold underline text-zinc-300">
+          Hello world!
+        </h1>
+      </div>
+    );
+  }
+  
+  export default NavBar;
+  
