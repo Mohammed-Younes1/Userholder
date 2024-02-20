f9078c80be1351cae461540c46e0748ba4f4a49b
Mohammed Younes <mohammed.alyounes@gmail.com>
2024-02-20 14:45:49 +0300

    fixing the posts opening pose

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,7 +4,7 @@ import Comment from "./Comment";
 function Posts({ comments, postId, name }) {
   return (
     <div>
-      <div className="z-[60] absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex items-center justify-center inset-0">
+      <div className="z-[60] fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] flex items-center justify-center inset-0">
         <div className="w-[100%] bg-white shadow-md rounded-lg px-6 py-10">
           <Comment comments={comments} postId={postId} name={name} />
         </div>
